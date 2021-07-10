5724c344d1c3e95f2d12836f0fbaf96c3f327dfd
kong sovary <kong.sovary@gmail.com>
2021-07-10 11:49:57 +0700

    interstitial added

--- app/src/main/java/com/hellokh/sovary/adfbkotlin/MainActivity.kt
+++ app/src/main/java/com/hellokh/sovary/adfbkotlin/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.hellokh.sovary.adfbkotlin
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
 import com.facebook.ads.AdSize
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val ad = FbAdManager(this)
         ad.showBanner(findViewById(R.id.banner_wrapper),"IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID", AdSize.BANNER_HEIGHT_90)
+        ad.createInterstitial("IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID")
+        val btn = findViewById<Button>(R.id.btn_open)
+        btn.setOnClickListener {
+            val intent = Intent(this,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
